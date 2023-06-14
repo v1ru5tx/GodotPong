47bf413bd4e65067ab933bc669ea46ece81c2885
angel Andrade <abachavez@gmail.com>
2023-06-14 10:08:07 -0600

    Adding the ball

--- Player.gd
+++ Player.gd
@@ -2,7 +2,7 @@ extends KinematicBody2D
 
 var speed = 400
 
-func _physics_process(delta):
+func _physics_process(_delta):
 	var direction = Vector2.ZERO # Vector2(0,0)
 	if Input.is_action_pressed("ui_up"):
 		direction.y -= 1
@@ -10,5 +10,5 @@ func _physics_process(delta):
 	if Input.is_action_pressed("ui_down"):
 		direction.y += 1
 	
-	move_and_slide(speed * direction)
+	var _retorn = move_and_slide(speed * direction)
 	
